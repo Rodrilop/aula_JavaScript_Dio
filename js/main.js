b8d51b4c946da41b0f2455fd9b81b45e4325b7b2
Rodrilop <rodrigocslopes@hotmail.com>
2020-12-15 08:55:13 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,82 @@
+function clicou(){
+	document.getElementById ("Agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+//console.log(document.getElementById ("Agradecimento"));
+}
+
+function redirecionar(){
+	window.open("https://www.homehost.com.br/blog/tutoriais/html-basico/");
+	//window.location.href = "https://www.homehost.com.br/blog/tutoriais/html-basico/"
+}
+
+function trocar(){
+	document.getElementById ("mousemove").innerHTML = "<b>Obrigado por passar o mouse</b>";
+}
+
+
+/*function soma (n1, n2){
+	return n1 + n2;
+}
+
+function validaIdade (idade){
+	var validar;
+	if (idade >= 18){
+		validar = true
+	}else{
+		validar = false
+	}
+	return validar;
+}
+
+//alert (soma(5, 10));
+
+
+console.log (validaIdade(idade));
+*/
+
+//var d = new Date();
+//alert(d);
+
+/*var count;
+
+for (count = 0; count <= 5; count++){
+	alert(count);
+};
+*/
+
+/*var count = 0;
+while (count <= 5){
+	console.log (count);
+	count +=1;
+};
+*/
+
+//var idade = prompt("Qual a sua idade?");
+//	alert ("Maior de idade.")
+//} else {
+//	alert ("Menor de idade.")
+//};
+
+
+
+//var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+//console.log (frutas);
+//alert (frutas[1].nome);
+
+//var lista = ["maça", "pêra", "laranja"]
+//lista.push ("uva");
+//lista.pop ("laranja");
+//console.log (lista);
+//console.log (lista.toString());
+//console.log (lista.join(" - "));
+
+
+
+//var nome = "Rodrigo Lopes"
+//var idade = 36;
+//var idade2 = 10;
+//var frase = "O Corinthians é o melhor time do mundo.";
+//alert (nome + " tem " + idade + " anos.");
+//alert (idade + idade2);
+//console.log (idade + idade2);
+//console.log (frase.toUpperCase());
+//alert(frase.replace("Corinthians", "Brasil"));
